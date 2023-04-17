e5a910e11c7c17295460f3b7d031dcc521737197
gertelrina <gertelrina@yandex.ru>
2023-04-17 22:39:10 +0000

    fix linter3

--- include/Automata.h
+++ include/Automata.h
@@ -10,22 +10,22 @@ enum STATES { OFF, WAIT, ACCEPT, CHECK, COOK };
 
 class Automata {
   private:
-  double cash;
-  std::vector<std::string> menu;
-  std::vector<double> price;
-  STATES state;
-  void cook();
-  void finish();
+    double cash;
+    std::vector<std::string> menu;
+    std::vector<double> price;
+    STATES state;
+    void cook();
+    void finish();
 
   public:
-  Automata();
-  void on();
-  void off();
-  void coin(double);
-  std::vector<std::string> getMenu();
-  STATES getState();
-  void choice();
-  void check(int);
-  void cancel();
+    Automata();
+    void on();
+    void off();
+    void coin(double);
+    std::vector<std::string> getMenu();
+    STATES getState();
+    void choice();
+    void check(int);
+    void cancel();
 };
 #endif // INCLUDE_AUTOMATA_H_"
